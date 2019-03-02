8459acb18b16ff75390c6111f3e99e53d511353c
Philippe Recto <precto1285@gmal.com>
2019-03-01 21:51:09 -0800

    created state

--- react-list-organizer/src/App.js
+++ react-list-organizer/src/App.js
@@ -5,6 +5,27 @@ import Grocery from './Components/2Component/Grocery';
 import Todo from './Components/2Component/To-do';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      grocery: [
+        { fruit: ['apple', 'banana', 'orange', 'melon'] },
+        { starch: ['bread', 'rice', 'potato', 'corn'] },
+        { meat: ['beef', 'chicken', 'pork', 'fish'] },
+        { vegetable: ['spinach', 'brocolli', 'green beans', 'asparagus'] }
+      ],
+      todo: [
+        { office: ['file report', 'team meeting', 'client meeting', 'planning'] },
+        { home: ['clean the carpet', 'take out the trash', 'wash the dishes'] },
+        { dog: ['morning walk', 'grooming', 'play time', 'afternoon walk', 'medicine'] },
+        { friends: ['dinner', 'bowling', 'yoga class', 'golf'] },
+        { family: ['Nephews Birthday', 'Aunties Funeral', 'Daughters Wedding'] }
+      ]
+    }
+
+
+
+  }
   render() {
     return (
       <div className="App">
